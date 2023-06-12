7d08f027198d86605c4482b69e8127e3ebeaf22b
Yimashenka <117358924+Yimashenka@users.noreply.github.com>
2023-06-12 12:06:30 +0200

    Add examples part

---
+++ examples/examples.py
@@ -0,0 +1,9 @@
+from Search.search import *
+
+# SEARCH
+
+## Lineary Search
+not_sorted_array = [-12, 15, 156, 8, -17, -6, 2, 1, 59, -85]
+
+print(linear_search(not_sorted_array, len(not_sorted_array), 2))
+print(linear_search_v2(not_sorted_array, 0))
